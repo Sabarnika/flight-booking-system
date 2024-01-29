a7b03d68687521fbf2afbbdb9f730b30882eb10c
Sabarnika <sabarnika1234@gmail.com>
2024-01-29 22:42:35 +0530

    commit-1

--- frontend/src/screens/List.js
+++ frontend/src/screens/List.js
@@ -38,7 +38,7 @@ function List() {
   }, []);
   return (
     <div>
-      <h2>User Bookings</h2>
+      <h2>User Booking</h2>
       <ul>
         {bookings.map((booking) => (
           <li key={booking.date}>
