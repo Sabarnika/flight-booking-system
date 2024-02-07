dbab9ee14c7527c5a559c29b12ac35538715a861
Sabarnika <98590604+Sabarnika@users.noreply.github.com>
2024-02-07 09:18:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,67 +1,9 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-// const cors = require("cors");
-// const app = express();
-// // const allowedOrigins = ['http://localhost:3000',"https://flight-booking-system-hd9i.vercel.app"];
-
-// // const corsOptions = {
-// //   origin: function(origin, callback) {
-// //     if (!origin || allowedOrigins.includes(origin)) {
-// //       callback(null, true);
-// //     } else {
-// //       callback(new Error('Not allowed by CORS'));
-// //     }
-// //   },
-// //   methods: ['GET', 'PUT', 'POST', 'DELETE'],
-// //   allowedHeaders: ['Content-Type'],
-// //   credentials: true
-// // };
-
-// // app.use(cors(corsOptions));
-// // app.use(express.json());
-// // app.use(express.urlencoded({ extended: true }));
-// // const userRoute = require("./Routes/userRoute");
-// // const airlineRoute = require("./Routes/airlineRoute");
-// // const scheduleRoute = require("./Routes/scheduleRoute");
-// // const bookingRouter=require('./Routes/bookingRoute')
-// // dotenv.config();
-// // app.use(express.json())
-// // mongoose
-// //   .connect(process.env.MONGO_URL)
-// //   .then(() => {
-// //     console.log("DB Connected");
-// //   })
-// //   .catch((err) => {
-// //     console.log(err);
-// //   });
-// // app.use("/user", userRoute);
-// // app.use("/airline", airlineRoute);
-// // app.use("/admin",scheduleRoute);
-// // app.use("/customer",bookingRouter)
-// app.get("/",(req,res)=>{
-//     res.send("This is the a backend server");
-// })
-// app.get("/users",(req,res)=>{
-//     res.send("This is the for server");
-// })
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Server listening at PORT ${PORT}`);
-// });
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
-dotenv.config();
-
-// Enable CORS for all routes
-// app.use(cors());
-
-// Set up headers
-const allowedOrigins = ['http://localhost:3000',"https://expense-tracker-bice-six.vercel.app"];
-
+const allowedOrigins = ['http://localhost:3000',"https://flight-booking-system-hd9i.vercel.app"];
 const corsOptions = {
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -74,22 +16,15 @@ const corsOptions = {
   allowedHeaders: ['Content-Type'],
   credentials: true
 };
-
 app.use(cors(corsOptions));
-
-
-// Set up headers
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://expense-tracker-bice-six.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", "true"); // Add this line if your request includes credentials (like cookies)
-//   next();
-// });
-
-
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+const userRoute = require("./Routes/userRoute");
+const airlineRoute = require("./Routes/airlineRoute");
+const scheduleRoute = require("./Routes/scheduleRoute");
+const bookingRouter=require('./Routes/bookingRoute')
+dotenv.config();
+app.use(express.json())
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -98,14 +33,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
- app.get("/",(req,res)=>{
+app.use("/user", userRoute);
+app.use("/airline", airlineRoute);
+app.use("/admin",scheduleRoute);
+app.use("/customer",bookingRouter)
+app.get("/",(req,res)=>{
     res.send("This is the a backend server");
-  })
-  app.get("/users",(req,res)=>{
+})
+app.get("/users",(req,res)=>{
     res.send("This is the for server");
-  })
-  
-const PORT = process.env.PORT || 8080;
+})
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+  console.log(`Server listening at PORT ${PORT}`);
 });
